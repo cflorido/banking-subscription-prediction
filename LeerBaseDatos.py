352cee2196e723d4644dc47518607c76b200b671
Alejaaag <aleja.garzon2106@gmail.com>
2024-11-04 07:23:03 -0500

    Ubuntu y rectifcar funcionamiento

--- LeerBaseDatos.py
+++ LeerBaseDatos.py
@@ -6,3 +6,14 @@ engine = psycopg2.connect(
     host="database-2.cw2fwyuh7xdb.us-east-1.rds.amazonaws.com", 
     port='5432' 
 )
+
+
+#### UBUNTU
+# Conectar a la base de datos
+engine = psycopg2.connect(
+    dbname="prod", 
+    user="postgres", 
+    password="manolo21", 
+    host="database-2.cw2fwyuh7xdb.us-east-1.rds.amazonaws.com", 
+    port="5432"
+)
