b84bb6e60fa60492b7edb06c0f78644433696b77
Alejaaag <aleja.garzon2106@gmail.com>
2024-11-05 02:31:34 -0500

    fin

--- solorectificar_app.py
+++ solorectificar_app.py
@@ -77,4 +77,4 @@ def update_output_div(marital_status, campaign_response):
 
 # Ejecutar la aplicación
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, port=8050)
